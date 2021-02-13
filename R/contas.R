1959a9117dfbdfc075d823df39ec9c669a47d0ed
mecarolme <carolyn3h0@gmail.com>
2021-02-12 22:26:42 -0300

    aula3

---
+++ R/contas.R
@@ -0,0 +1,32 @@
+#' Function somar
+#'
+#' This function is used to add two values.
+#'
+#' @param a first number.
+#' @param b another number.
+#'
+#' @examples
+#' sum(100, 50)
+#' Console response: 150
+#'
+#' @export
+somar <- function(a,b) {
+  a + b
+}
+
+#' Function subtract
+#'
+#' This function is used to subtract two values.
+#'
+#' @param a first number.
+#' @param b another number.
+#'
+#' @examples
+#' subtract(100, 50)
+#' Console response: 50
+#'
+#' @export
+subtract <- function(a,b) {
+  a - b
+}
+
